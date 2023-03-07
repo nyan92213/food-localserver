1348647552403d5f6f28dacc330517f1a510d702
nyan92213 <ivanbezrukov92213@gmail.com>
2023-03-07 12:47:10 +0300

    using fetch for requests + analogue as axios

--- js/script.js
+++ js/script.js
@@ -159,72 +159,94 @@
     //=================================================================================
 
     class MenuCard {
-        constructor(pic, alt, menuName, about, price, parentSelector,  ...classes) {
-            this.pic = pic
-            this.alt = alt
-            this.menuName = menuName
-            this.about = about
-            this.price = price
-            this.classes = classes
-            this.parent = document.querySelector(parentSelector)
-            this.transfer = 80
-            this.changeToRub()
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH(); 
         }
 
-        changeToRub() {
-            this.price = this.price * this.transfer
+        changeToUAH() {
+            this.price = this.price * this.transfer; 
         }
 
         render() {
-            const element = document.createElement('div')
+            const element = document.createElement('div');
+
             if (this.classes.length === 0) {
-                this.element = 'menu__item'
-                element.classList.add(this.element)
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
             } else {
-            this.classes.forEach(className => element.classList.add(className))
+                this.classes.forEach(className => element.classList.add(className));
             }
 
-            element.innerHTML += `
-                <img src=${this.pic} alt="vegy">
-                <h3 class="menu__item-subtitle">Меню ${this.menuName}</h3>
-                <div class="menu__item-descr">${this.about}</div>
+            element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
                 <div class="menu__item-divider"></div>
                 <div class="menu__item-price">
                     <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
-                `
-                this.parent.append(element)
+            `;
+            this.parent.append(element);
         }
     }
 
-    new MenuCard('"img/tabs/vegy.jpg"',
-    'vegy',
-    '"Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 
-    9, 
-    '.menu .container',
-    // 'menu__item',
-    // 'big'
-    ).render()
+    const getResource = async (url) => { // async позволяет дождаться результата запроса
+        const res = await fetch(url)
 
-    new MenuCard('"img/tabs/elite.jpg"',
-    'elite',
-    '“Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 
-    15, 
-    '.menu .container',
-    'menu__item'
-    ).render()
+        if (!res.ok) {
+            throw new Error(`couldn't fetch ${url}, status: ${res.status}`) // конструирование выкидывания ошибки
+        }
+        
+        return await res.json()
+    }
 
-    new MenuCard('"img/tabs/post.jpg"',
-    'post',
-    '"Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 
-    10, 
-    '.menu .container',
-    'menu__item'
-    ).render()
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({img, altimg, title, descr, price}) => {
+    //             new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+    //         });
+    //     });
+
+    axios.get('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+            });
+        })
+
+    // getResource('http://localhost:3000/menu')
+    //         .then(data => createCard(data))
+
+    // function createCard(data) {
+    //     data.forEach(({img, altimg, title, descr, price}) => {
+    //         const element = document.createElement('div')
+    //         price *= 27
+
+    //         element.classList.add('menu__item')
+
+    //         element.innerHTML = `                
+    //             <img src=${img} alt=${altimg}>
+    //             <h3 class="menu__item-subtitle">${title}</h3>
+    //             <div class="menu__item-descr">${descr}</div>
+    //             <div class="menu__item-divider"></div>
+    //             <div class="menu__item-price">
+    //                 <div class="menu__item-cost">Цена:</div>
+    //                 <div class="menu__item-total"><span>${price}</span> грн/день</div>
+    //             </div>
+    //         `
+
+    //         document.querySelector('.menu .container').append(element)
+    //     })
+    // }
 
     // работа с формами отправки
 
@@ -236,10 +258,22 @@
     }
 
     forms.forEach(item => {
-        postData(item)
+        bindPostData(item)
     })
 
-    function postData(form) {
+    const postData = async (url, data) => { // async позволяет дождаться результата запроса
+        const res = await fetch(url, { // ждем окончания завпроса, который в await
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data
+        })
+        
+        return await res.json()
+    }
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault()
 
@@ -253,21 +287,12 @@
 
             const formData = new FormData(form)
 
-            const object = {}
-            formData.forEach(function(value, key) {
-                object[key] = value
-            })
+            const json = JSON.stringify(Object.fromEntries(formData.entries())) // берем формдату, превращаем в массив масивов, потом в классический объект и уже его превращаем в json
 
             // Fetch API using
 
-            fetch('server.php', {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            }).then(data => data.text()
-            ).then(data => {
+            postData('http://localhost:3000/requests', json)
+            .then(data => {
                 console.log(data)
                 showThanksModal(message.success)
                 statusMessage.remove()
@@ -315,9 +340,9 @@
     //   .then(response => response.json()) // здесь возвращается промис
     //   .then(json => console.log(json))
 
-    fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res))
+    // fetch('http://localhost:3000/menu')
+    //     .then(data => data.json())
+    //     .then(res => console.log(res))
 
     
  })
